83f27f8fe5927f94f088246eff538988d6833b03
user1 <user1@example.org>
2023-09-01 05:38:37 -0700

    Initial commit

---
+++ src/main/java/com/example/kerberosAuth/KerberosAuthenticatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.kerberosAuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KerberosAuthenticatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KerberosAuthenticatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kerberosAuth/KerberosAuthenticatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kerberosAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KerberosAuthenticatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
